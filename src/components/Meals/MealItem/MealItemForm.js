ff1e1c94a02d870b788015adc6888848aa5b267b
James McCarron <mccarron.j@gmail.com>
2021-06-23 13:25:57 +1000

    New form and styling in MealItemForm

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -2,8 +2,13 @@ import React from "react";
 
 import classes from "./MealItemForm.module.css";
 
-const MealItemForm = () => {
-  return <div></div>;
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.form}>
+      <input />
+      <button>+ Add</button>
+    </form>
+  );
 };
 
 export default MealItemForm;
